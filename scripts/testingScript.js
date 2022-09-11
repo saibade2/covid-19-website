acdfcfc36886af92443ba3e67d47c7967600b5ea
saibade2 <90086494+saibade2@users.noreply.github.com>
2022-09-11 15:59:44 +0530

    Add files via upload

---
+++ scripts/testingScript.js
@@ -0,0 +1,10 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyAsBMd0Wn8pKpHz0Hva3Swm2jdn95DKVpk",
+    authDomain: "corona-testing-9b7e8.firebaseapp.com",
+    databaseURL: "https://corona-testing-9b7e8-default-rtdb.firebaseio.com",
+    projectId: "corona-testing-9b7e8",
+    storageBucket: "corona-testing-9b7e8.appspot.com",
+    messagingSenderId: "220013195925",
+    appId: "1:220013195925:web:9e4afdb317534ff7c2219c"
+  };
+  firebase.initializeApp(firebaseConfig);
